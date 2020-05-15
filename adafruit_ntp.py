772dab6ec6ec886be99ad77f8e0037e4377119c4
Jesse McKenna <51097226+theelectricmayhem@users.noreply.github.com>
2020-05-14 21:03:58 -0600

    Black reformatting

--- adafruit_ntp.py
+++ adafruit_ntp.py
@@ -22,22 +22,14 @@
 """
 `adafruit_ntp`
 ================================================================================
-
 Network Time Protocol (NTP) helper for CircuitPython
-
-
 * Author(s): Brent Rubell
-
 Implementation Notes
 --------------------
-
 **Hardware:**
-
 **Software and Dependencies:**
-
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
-
 """
 import time
 import rtc
@@ -49,7 +41,6 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_NTP.git"
 class NTP:
     """Network Time Protocol (NTP) helper module for CircuitPython.
     This module does not handle daylight savings or local time.
-
     :param adafruit_esp32spi esp: ESP32SPI object.
     """
 
@@ -64,12 +55,11 @@ class NTP:
     def set_time(self, tz_offset=0):
         """Fetches and sets the microcontroller's current time
         in seconds since since Jan 1, 1970.
-
         :param int tz_offset: The offset of the local timezone,
         in seconds west of UTC (negative in most of Western Europe,
         positive in the US, zero in the UK).
         """
-        
+
         try:
             now = self._esp.get_time()
             now = time.localtime(now[0] + tz_offset)
