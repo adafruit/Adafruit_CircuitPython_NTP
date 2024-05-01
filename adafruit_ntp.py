04c171ee74d7b9eefc08e9f8126752a1d29918e2
Justin Myers <justmobilize@users.noreply.github.com>
2024-04-30 21:49:39 -0700

    Add ESP32SPI support

--- adafruit_ntp.py
+++ adafruit_ntp.py
@@ -22,10 +22,14 @@ Implementation Notes
 import struct
 import time
 
+from micropython import const
+
+
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_NTP.git"
 
 NTP_TO_UNIX_EPOCH = 2208988800  # 1970-01-01 00:00:00
+PACKET_SIZE = const(48)
 
 
 class NTP:
@@ -53,9 +57,8 @@ class NTP:
         :param int socket_timeout: UDP socket timeout, in seconds.
         """
         self._pool = socketpool
-        self._server = server
-        self._port = port
-        self._packet = bytearray(48)
+        self._socket_address = self._pool.getaddrinfo(server, port)[0][4]
+        self._packet = bytearray(PACKET_SIZE)
         self._tz_offset = int(tz_offset * 60 * 60)
         self._socket_timeout = socket_timeout
 
@@ -72,20 +75,26 @@ class NTP:
         is received within socket_timeout seconds"""
         if time.monotonic_ns() > self.next_sync:
             self._packet[0] = 0b00100011  # Not leap second, NTP version 4, Client mode
-            for i in range(1, len(self._packet)):
+            for i in range(1, PACKET_SIZE):
                 self._packet[i] = 0
             with self._pool.socket(self._pool.AF_INET, self._pool.SOCK_DGRAM) as sock:
                 sock.settimeout(self._socket_timeout)
-                sock.sendto(self._packet, (self._server, self._port))
-                sock.recvfrom_into(self._packet)
+                if hasattr(sock, "sendto"):
+                    sock.sendto(self._packet, self._socket_address)
+                    sock.recvfrom_into(self._packet)
+                else:
+                    sock.connect(
+                        self._socket_address,
+                        conntype=self._pool._interface.UDP_MODE,  # pylint: disable=protected-access
+                    )
+                    sock.send(self._packet)
+                    self._packet[:PACKET_SIZE] = sock.recv(PACKET_SIZE)
                 # Get the time in the context to minimize the difference between it and receiving
                 # the packet.
                 destination = time.monotonic_ns()
             poll = struct.unpack_from("!B", self._packet, offset=2)[0]
             self.next_sync = destination + (2**poll) * 1_000_000_000
-            seconds = struct.unpack_from(
-                "!I", self._packet, offset=len(self._packet) - 8
-            )[0]
+            seconds = struct.unpack_from("!I", self._packet, offset=PACKET_SIZE - 8)[0]
             self._monotonic_start = (
                 seconds
                 + self._tz_offset
